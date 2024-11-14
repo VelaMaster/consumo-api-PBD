2906e77cd96cb083fee708520b0a649505ec3599
VelaMaster <pdiegovela@gmail.com>
2024-11-14 12:15:13 -0600

    Commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-
+import { UserService } from './services/user.service';
+import { UserListComponent } from './components/user-list/user-list.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
+declarations: [
+AppComponent,
+UserListComponent
+],
+imports: [
+BrowserModule,
+HttpClientModule,
+NgxPaginationModule
+],
+providers: [UserService],
+bootstrap: [AppComponent]
 })
 export class AppModule { }
